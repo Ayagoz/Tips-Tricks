970466119949e4f250ad2d6da1c10cff69cdea83
Ayagoz <ayagoz.musabaeva@mail.ru>
2020-02-29 08:58:42 +0300

    feat: Add train script

--- project/cnd/ocr/train_script.py
+++ project/cnd/ocr/train_script.py
@@ -1,97 +1,95 @@
 import argparse
-import os
 
-from torch.utils.data import DataLoader, ConcatDataset
-
+from torch.utils.data import DataLoader
+from argus.callbacks import MonitorCheckpoint
 from cnd.ocr.dataset import OcrDataset
-from cnd.ocr.model import CRNN
+from cnd.ocr.argus_model import CRNNModel
+from cnd.config import OCR_EXPERIMENTS_DIR, CONFIG_PATH, Config
+from cnd.ocr.utils import regular, negative
+from cnd.ocr.transforms import get_transforms
+from cnd.ocr.metrics import StringAccuracy
 import string
+from pathlib import Path
 import torch
 
 torch.backends.cudnn.enabled = False
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-en", "--experimet_name", help="Save folder name", required=True)
+parser.add_argument("-en", "--experiment_name", help="Save folder name", required=True)
 parser.add_argument("-gpu_i", "--gpu_index", type=str, default="0", help="gpu index")
 args = parser.parse_args()
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_index
+# IF YOU USE GPU UNCOMMENT NEXT LINES:
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_index
 
-#define experiment path
-# EXPERIMENT_NAME = args.experimet_name
-# EXPERIMENT_DIR = OCR_EXPERIMENTS_DIR / EXPERIMENT_NAME
+# define experiment path
+EXPERIMENT_NAME = args.experiment_name
+EXPERIMENT_DIR = OCR_EXPERIMENTS_DIR / EXPERIMENT_NAME
+
+CV_CONFIG = Config(CONFIG_PATH)
 
 DATASET_PATHS = [
-    "ds1"
+    Path(CV_CONFIG.get("data_path"))
 ]
+# CHANGE YOUR BATCH SIZE
+BATCH_SIZE = 100
+# 400 EPOCH SHOULD BE ENOUGH
+NUM_EPOCHS = 400
 
-BATCH_SIZE = 512
-
-# CV_CONFIG =
-
-alphabet = ":,.; "
+alphabet = " "
 alphabet += string.ascii_uppercase
 alphabet += "".join([str(i) for i in range(10)])
 
-PARAMS = {
-    "model": (
-        "CRNN",
-        {
-            "image_height": 10,
-            "number_input_channels": 1,
-            "number_class_symbols": 32,
-            "rnn_size": 128,
-        },
-    ),
-    "loss": {},
-    "optimizer": ("Adam", {"lr": 0.001}),
-    "alphabet": alphabet,
-    "device": "cuda",
-}
+MODEL_PARAMS = {"nn_module":
+                    ("CRNN", {
+                    }),
+                "alphabet": alphabet,
+                "loss": {},
+                "optimizer": ("Adam", {"lr": 0.0001}),
+                # CHANGE DEVICE IF YOU USE GPU
+                "device": "cpu",
+                }
 
 if __name__ == "__main__":
-    # if EXPERIMENT_DIR.exists():
-    #     print(f"Folder 'EXPERIMENT_DIR' already exists")
-    # else:
-    #     EXPERIMENT_DIR.mkdir(parents=True, exist_ok=True)
+    if EXPERIMENT_DIR.exists():
+        print(f"Folder 'EXPERIMENT_DIR' already exists")
+    else:
+        EXPERIMENT_DIR.mkdir(parents=True, exist_ok=True)
 
-    # transforms =  # define your transforms here
+    transforms =  #TODO: define your transforms here
     # define data path
-    train_dataset_paths = [p / "train" for p in DATASET_PATHS]
-    train_datasets = [
-        OcrDataset(p)
-        for p in train_dataset_paths
-    ]
 
-    train_dataset = ConcatDataset(train_datasets)
+    train_dataset = # define your dataset
 
     train_loader = DataLoader(
         train_dataset,
         batch_size=BATCH_SIZE,
         shuffle=True,
         drop_last=True,
-        num_workers=10,
+        num_workers=1,
     )
+    # IT IS BETTER TO SPLIT DATA INTO TRAIN|VAL AND USE METRICS ON VAL
+    # val_dataset_paths = [p / "val" for p in DATASET_PATHS]
+    # val_dataset = ConcatDataset([OcrDataset(p) for p in val_dataset_paths])
+    #
+    # val_loader = DataLoader(
+    #     val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4
+    # )
 
-    val_dataset_paths = [p / "val" for p in DATASET_PATHS]
-    val_dataset = ConcatDataset([OcrDataset(p) for p in val_dataset_paths])
+    model = CRNNModel(MODEL_PARAMS)
+    # YOU CAN ADD CALLBACK IF IT NEEDED, FIND MORE IN argus.callbacks
+    callbacks = [
+        MonitorCheckpoint(EXPERIMENT_DIR, monitor="train_loss", max_saves=6),
+    ]
+    # YOU CAN IMPLEMENT DIFFERENT METRICS AND USE THEM TO SEE HOW MANY CORRECT PREDICTION YOU HAVE
+    # metrics = [StringClassAccuracy()]
 
-    val_loader = DataLoader(
-        val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4
-    )
-
-    model = CRNN(PARAMS)
-    #define callbacks if any
-    callbacks = []
-    #define metrics if any
-    metrics = []
-
-    fit(model,
+    model.fit(
         train_loader,
-        val_loader=val_loader,
-        max_epochs=1500,
-        metrics=metrics,
+        # val_loader=val_loader,
+        max_epochs=NUM_EPOCHS,
+        # metrics=metrics,
         callbacks=callbacks,
         metrics_on_train=True,
     )
