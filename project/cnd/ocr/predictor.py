10f9a1e40babb92a28e7e2cefc95ca8080254f9c
Ayagoz <ayagoz.musabaeva@mail.ru>
2020-03-02 21:15:45 +0300

    fix: Add converter

--- project/cnd/ocr/predictor.py
+++ project/cnd/ocr/predictor.py
@@ -3,13 +3,15 @@ from argus.model import load_model
 
 
 class Predictor:
-    def __init__(self, model_path, image_size, device="cuda"):
+    def __init__(self, model_path, image_size, converter, device="cuda"):
         self.model = load_model(model_path, device=device)
         self.ocr_image_size = image_size
         self.transform =  #TODO: prediction_transform
+        self.converter = converter
 
     def predict(self, images):
         #TODO: check for correct input type, you can receive one image [x,y,3] or batch [b,x,y,3]
         images = self.transform(images)
         pred = self.model.predict({"image": images})
-        return pred
+        text = self.converter(pred)
+        return text
