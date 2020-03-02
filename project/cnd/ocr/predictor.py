be20f52e05934d42d4406f609b3d9ddff71ecd43
Ayagoz <ayagoz.musabaeva@mail.ru>
2020-03-02 21:07:21 +0300

    fix: Add converter

--- project/cnd/ocr/predictor.py
+++ project/cnd/ocr/predictor.py
@@ -3,14 +3,16 @@ import torch
 
 
 class Predictor:
-    def __init__(self, model_path, image_size, device="cuda"):
+    def __init__(self, model_path, image_size, converter, device="cuda"):
 
         self.model = torch.load(model_path).to(device)
         self.ocr_image_size = image_size
         self.transform =  #TODO: prediction_transform
+        self.converter = converter
 
     def predict(self, images):
         #TODO: check for correct input type, you can receive one image [x,y,3] or batch [b,x,y,3]
         images = self.transform(images)
         pred = self.model.predict({"image": images})
-        return pred
+        text = self.converter(pred)
+        return text
